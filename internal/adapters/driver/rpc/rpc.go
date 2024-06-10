0f27ba11b8ee81abfe6bba20fafd5e1b3ff0ee03
Vasiliy Novikov <v.y.novikov@gmail.com>
2024-06-10 20:03:25 +0300

    kafka in docker-compose tested

--- internal/adapters/driver/rpc/rpc.go
+++ internal/adapters/driver/rpc/rpc.go
@@ -3,10 +3,8 @@ package rpc
 
 import (
 	"context"
-	"fmt"
 	"net"
 	"os"
-	"strings"
 	"sync"
 	"time"
 
@@ -29,8 +27,9 @@ type Receiver interface {
 func NewReceiver(a application.Application) *ReceiverStruct {
 
 	var (
-		conn *kafka.Conn
-		err  error
+		conn       *kafka.Conn
+		err        error
+		partitions []kafka.Partition
 	)
 
 	kafkaAddr := os.Getenv("KAFKA_ADDR")
@@ -39,57 +38,55 @@ func NewReceiver(a application.Application) *ReceiverStruct {
 	dialURI := net.JoinHostPort(kafkaAddr, kafkaPort)
 
 	kafkaTopic := os.Getenv("KAFKA_TOPIC")
-	kafkaConsumerGroupID := os.Getenv("KAFKA_CONSUMER_GROUP_ID")
 
-	logger.L.Infof("in rpc.NewReceiver addr = %s, topic = %s\n", dialURI, kafkaTopic)
+	dialURI = net.JoinHostPort(kafkaAddr, kafkaPort)
 
 	for i := 0; i < 5; i++ {
-
-		logger.L.Infof("in rpc.NewReceiver %d attempt to dial to %s\n", i, dialURI)
 
 		conn, err = kafka.Dial("tcp", dialURI)
 		if err != nil {
 
-			logger.L.Errorf("in rpc.NewReceiver cannot dial: %v\n", err)
+			logger.L.Errorf("in rpc.NewReceiver cannot dial: %v. Trying again\n", err)
+
 			time.Sleep(time.Second * 10)
 
 			continue
 		}
-	}
-	if conn == nil {
 
-		logger.L.Errorln("in rpc.NewReceiver failed to establish conn")
-		os.Exit(1)
-	}
+		partitions, err = conn.ReadPartitions()
 
-	partitions, err := conn.ReadPartitions()
-	if err != nil {
+		if err != nil {
 
-		logger.L.Errorln(err)
-		os.Exit(1)
-	}
+			time.Sleep(time.Second * 10)
 
-	for _, p := range partitions {
+			continue
 
-		if p.Topic == kafkaTopic {
+		}
 
-			logger.L.Infof("in rpc.NewReceiver topic %s is found\n", kafkaTopic)
+		for _, p := range partitions {
+
+			if p.Topic == kafkaTopic {
+
+				conn.Close()
+
+				rs := &ReceiverStruct{
+
+					A: a,
+					R: kafka.NewReader(kafka.ReaderConfig{
+						Brokers: []string{dialURI},
+						Topic:   kafkaTopic,
+						GroupID: "0",
+					}),
+				}
+
+				logger.L.Infof("in rpc.NewReceiver rs: %v\n", rs.R)
+
+				return rs
+			}
 		}
 	}
 
-	r := kafka.NewReader(kafka.ReaderConfig{
-		Brokers:  []string{fmt.Sprintf("%s:%s", kafkaAddr, kafkaPort)},
-		GroupID:  kafkaConsumerGroupID,
-		Topic:    kafkaTopic,
-		MaxBytes: 10e6, // 10MB
-	})
-
-	rcvr := &ReceiverStruct{
-		A: a,
-		R: r,
-	}
-
-	return rcvr
+	return &ReceiverStruct{}
 }
 
 func (r *ReceiverStruct) Run() {
@@ -101,28 +98,17 @@ func (r *ReceiverStruct) Run() {
 
 		if err != nil {
 
-			// TODO fix ERROR[06.08.2024 18:17:20] in rpc.Run cannot read from kafka: failed to dial:
-			//failed to open connection to [http://localhost:29092]:9092: dial tcp: lookup http://localhost:29092: no such host
-
-			logger.L.Errorf("in rpc.Run cannot read from kafka: %v\n", err)
-			if strings.Contains(err.Error(), "connection refused") {
-
-				time.Sleep(time.Second * 5)
-				r = NewReceiver(r.A)
-				continue
-			}
+			logger.L.Errorf("in rpc.Run cannot read from kafka: %v receiver: %v\n", err, r.R)
 		}
+
 		logger.L.Infof("in rpc.Run from message have read topic: %s, partition = %d, key: %q, value: %q\n", m.Topic, m.Partition, m.Key, m.Value)
 
-		//	if err := r.R.CommitMessages(context.Background(), m); err != nil {
-		//		logger.L.Errorf("in rpc.Run failed to commit messages: %v\n", err)
-		//	}
 		header, body := &pb.MessageHeader{}, &pb.MessageBody{}
 
 		if err = proto.Unmarshal(m.Key, header); err != nil {
 			logger.L.Errorf("in rpc.Run failed to unmarshal: %v\n", err)
 		}
-		logger.L.Infof("in rpc.Run unmarshalled body: %v\n", header)
+		logger.L.Infof("in rpc.Run unmarshalled header: %v\n", header)
 
 		if err = proto.Unmarshal(m.Value, body); err != nil {
 			logger.L.Errorf("in rpc.Run failed to unmarshal: %v\n", err)
